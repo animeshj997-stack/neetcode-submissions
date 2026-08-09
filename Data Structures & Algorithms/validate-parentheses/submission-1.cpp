8e0cc764c04c728291255fc089ed822a4e916d81
Animesh Jain <animeshj996@gmail.com>
2026-08-10 00:35:14 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char,char> mp;
+        mp[']']='[';
+        mp['}']='{';
+        mp[')']='(';
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+              st.push(s[i]);
+            else if(!st.empty() && mp[s[i]]==st.top()){
+                st.pop();
+            }
+            else{
+                return 0;
+            }
+        }
+        cout<<st.size();
+        return (st.size()==0);
+    }
+};
