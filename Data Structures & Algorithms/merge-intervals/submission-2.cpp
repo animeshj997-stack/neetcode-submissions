93174a8cd71650ed8c96537d64583c1f7ce0d90f
Animesh Jain <animeshj996@gmail.com>
2026-08-10 16:21:23 +0530

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(),intervals.end());
+
+        for(auto i : intervals){
+            if(res.empty()){
+                res.push_back(i);
+            }
+            else{
+                if(res.back()[1]>=i[0]){
+                    res.back()[1]=max(res.back()[1],i[1]);
+                }
+                else{
+                    res.push_back(i);
+                }
+            }
+        }
+
+        return res;
+    }
+};
